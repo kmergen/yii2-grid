f15ff29d3a1ee44513fdd516c48c3eed43dcaacb
Klaus Mergen <klaus.mergen@web.de>
2020-04-18 14:31:35 +0200

    add svg feather icons as icon for view edit and delete remove font-awesome icons

--- ActionColumn.php
+++ ActionColumn.php
@@ -31,14 +31,18 @@ use yii\helpers\Html;
  */
 class ActionColumn extends \yii\grid\ActionColumn
 {
+    public $viewIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye icon-grid-action-column"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg>';
+    public $editIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit icon-grid-action-column"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>';
+    public $deleteIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 icon-grid-action-column"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/><line x1="10" y1="11" x2="10" y2="17"/><line x1="14" y1="11" x2="14" y2="17"/></svg>';
+
     /**
      * Initializes the default button rendering callbacks.
      */
     protected function initDefaultButtons()
     {
-        $this->initDefaultButton('view', 'eye');
-        $this->initDefaultButton('update', 'edit');
-        $this->initDefaultButton('delete', 'trash', [
+        $this->initDefaultButton('view', $this->viewIcon);
+        $this->initDefaultButton('update', $this->editIcon);
+        $this->initDefaultButton('delete', $this->deleteIcon, [
             'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
             'data-method' => 'post',
         ]);
@@ -47,14 +51,14 @@ class ActionColumn extends \yii\grid\ActionColumn
     /**
      * Initializes the default button rendering callback for single button.
      * @param string $name Button name as it's written in template
-     * @param string $iconName The part of Bootstrap glyphicon class that makes it unique
+     * @param string $icon Svg icons feather icons. @see https://feathericons.com/
      * @param array $additionalOptions Array of additional options
      * @since 2.0.11
      */
-    protected function initDefaultButton($name, $iconName, $additionalOptions = [])
+    protected function initDefaultButton($name, $icon, $additionalOptions = [])
     {
         if (!isset($this->buttons[$name]) && strpos($this->template, '{' . $name . '}') !== false) {
-            $this->buttons[$name] = function ($url, $model, $key) use ($name, $iconName, $additionalOptions) {
+            $this->buttons[$name] = function ($url, $model, $key) use ($name, $icon, $additionalOptions) {
                 switch ($name) {
                     case 'view':
                         $title = Yii::t('yii', 'View');
@@ -73,9 +77,10 @@ class ActionColumn extends \yii\grid\ActionColumn
                     'aria-label' => $title,
                     'data-pjax' => '0',
                 ], $additionalOptions, $this->buttonOptions);
-                $icon = Html::tag('i', '', ['class' => "fas fa-$iconName"]);
+
                 return Html::a($icon, $url, $options);
             };
         }
     }
 }
+
